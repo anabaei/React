2823b87a6bc23bd7e680be5d90bdcf7456378bb8
anabaei <nabaei17@gmail.com>
2023-06-19 22:55:53 -0700

    add html

---
+++ nodejs/app.js
@@ -0,0 +1,23 @@
+import  express from 'express';
+
+import fs from 'fs'
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'view')));
+const htmlContent = fs.readFileSync('./views/home2.html', 'utf8');
+const aboutContent = fs.readFileSync('./views/about.html', 'utf8');
+
+app.get('/', function (req, res) {
+  res.send(htmlContent);
+});
+
+app.get('/about', function (req, res) {
+    res.send(aboutContent);
+  });
+
+app.listen(8000, '0.0.0.0', function () {
+  console.log('Server listening on port 8000');
+});
+
+module.exports = app;
