ff4e48b6654754771d2b64489771713037400dba
anabaei <nabaei17@gmail.com>
2023-06-22 14:15:06 -0700

    add acc

--- nodejs/app.js
+++ nodejs/app.js
@@ -4,13 +4,19 @@ import fs from 'fs'
 var app = express();
 var path = require('path');
 
+app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'view')));
 const htmlContent = fs.readFileSync('./views/home2.html', 'utf8');
 const aboutContent = fs.readFileSync('./views/about.html', 'utf8');
+const accordion = fs.readFileSync('./views/accordion.html', 'utf8');
 
 app.get('/', function (req, res) {
   res.send(htmlContent);
 });
+
+app.get('/acc', function (req, res) {
+    res.send(accordion);
+  });
 
 app.get('/about', function (req, res) {
     res.send(aboutContent);
